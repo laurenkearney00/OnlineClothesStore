ad54f57dfd7c75b0c8d65c1f76eb0cafb4494115
laurenkearney00 <c18330401@mytudublin.ie>
2022-03-29 23:01:32 +0100

    Created admin options class

---
+++ app/src/main/java/com/example/onlineclothesstore/AdminOptions.java
@@ -0,0 +1,28 @@
+package com.example.onlineclothesstore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class AdminOptions extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_options);
+
+    }
+}
