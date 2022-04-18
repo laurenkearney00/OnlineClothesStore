28b2a8f9ac90a2836d7d019e23146561a2071add
laurenkearney00 <c18330401@mytudublin.ie>
2022-04-18 18:01:03 +0100

    Created the online clothes store main activity

--- app/src/main/java/com/example/onlineclothesstore/MainActivity.java
+++ app/src/main/java/com/example/onlineclothesstore/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.onlineclothesstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void signInCustomer(View v) {
+        Intent intent = new Intent(getApplicationContext(), LoginCustomer.class);
+        startActivity(intent);
+    }
+
+    public void signInAdministrator(View v) {
+        Intent intent = new Intent(getApplicationContext(), LoginAdministrator.class);
+        startActivity(intent);
+    }
+
 }
